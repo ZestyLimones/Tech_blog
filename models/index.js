1a0c2c90b808fddbfb91fb7f1bfd2824c729c468
Micheal Limones <Micheal.Limones@du.edu>
2021-09-29 08:52:04 -0600

    commented out comments within index model.

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Blog = require('./Blog');
-const Comment = require('./Comment');
+// const Comment = require('./Comment');
 
 Blog.belongsTo(User, {
   foreignKey: 'user_id',
