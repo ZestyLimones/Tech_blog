e316446aaf127f89ebb8ce0c60170cc66a4adf26
Micheal Limones <Micheal.Limones@du.edu>
2021-09-29 10:51:23 -0600

    got html page to render. need to add correct data.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
     const blogs = blogData.map((blog) => blog.get({ plain: true }));
     res.render('homepage', {
       blogs,
-      logged_in: req.session.logged_in,
+      // logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
