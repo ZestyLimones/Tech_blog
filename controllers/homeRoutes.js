550349d28d5add65d938e4bf1917f4704a42e5cb
Micheal Limones <Micheal.Limones@du.edu>
2021-09-29 09:43:40 -0600

    added route to single blog post.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { Blog, User } = require('../models');
-const withAuth = require('../utils/auth');
+const { Blog, User, Comment } = require('../models');
+// const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
@@ -16,3 +16,24 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get('/blogs/:id', async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+    res.render('blog', {
+      ...blog,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
