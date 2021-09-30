a986a0455cecb81a7ecf0a4888984570ddbee13b
Micheal Limones <Micheal.Limones@du.edu>
2021-09-29 18:44:03 -0600

    added dashboard route. created dashboard handlebar and added content.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Blog, User, Comment } = require('../models');
-// const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
     const blogs = blogData.map((blog) => blog.get({ plain: true }));
     res.render('homepage', {
       blogs,
-      // logged_in: req.session.logged_in,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -40,7 +40,26 @@ router.get('/blogs/:id', async (req, res) => {
     const blog = blogData.get({ plain: true });
     res.render('blog', {
       ...blog,
-      // logged_in: req.session.logged_in,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Blog }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
