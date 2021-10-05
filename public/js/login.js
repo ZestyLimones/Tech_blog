28844dcc1d7e336e5aa4e2c2c040294cc61c57c9
Micheal Limones <michlimones@gmail.com>
2021-10-04 20:05:09 -0600

    updated alert messages.

--- public/js/login.js
+++ public/js/login.js
@@ -17,7 +17,7 @@ const loginFormHandler = async (event) => {
       // If successful, redirect the browser to the profile page
       document.location.replace('/dashboard');
     } else {
-      alert(response.statusText);
+      alert('unable to login. Please Try again');
     }
   }
 };
@@ -38,7 +38,7 @@ const signupFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/');
     } else {
-      alert(response.statusText);
+      alert('unable to signup. Please Try again');
     }
   }
 };
